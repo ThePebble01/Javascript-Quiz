6e55ecef5d78d730e1de322b7b5aed95e3229216
unknown <john.gordos@outlook.com>
2023-05-24 11:48:00 -0600

    finished cleaning .js for index and high-score page

--- assets/js/high-scores-script.js
+++ assets/js/high-scores-script.js
@@ -1,14 +1,21 @@
 var localStorageScoreKey = "jsQuizScores"; //......NEED TO LEARN ABOUT CORS BEFORE I CAN TRY TO IMPORT/EXPORT....
 var currentScores = JSON.parse(localStorage.getItem(localStorageScoreKey));
 var scoreTableEl = $("#high-scores");
-console.log(scoreTableEl);
 for (i = 0; i < currentScores.length; i++) {
   var visitorScore = currentScores[i];
-  var tableRowEl = scoreTableEl.insertRow();
-  var initialsTableDataEl = tableRowEl.insertCell();
-  initialsTableDataEl.textContent = visitorScore.initials;
-  var scoreTableDataEl = tableRowEl.insertCell();
-  scoreTableDataEl.textContent = visitorScore.score;
-  var quizDateTableDataEl = tableRowEl.insertCell();
-  quizDateTableDataEl.textContent = visitorScore.quizDate;
+  var tableRowEl = $("<tr>");
+
+  var initialsTableDataEl = $("<td>");
+  initialsTableDataEl.text(visitorScore.initials);
+  tableRowEl.append(initialsTableDataEl);
+
+  var scoreTableDataEl = $("<td>");
+  scoreTableDataEl.text(visitorScore.score);
+  tableRowEl.append(scoreTableDataEl);
+
+  var quizDateTableDataEl = $("<td>");
+  quizDateTableDataEl.text(visitorScore.quizDate);
+  tableRowEl.append(quizDateTableDataEl);
+
+  scoreTableEl.append(tableRowEl);
 }
--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -23,8 +23,108 @@ const quizQuestions = [
   ),
   new QuizQuestion(
     "Which method allows you to access an HTML element by its Id?  (No JQuery)",
-    ["$(\"#Id\")", "getElement(\"Id\")", "gimmeEl(\"#Id\")", "getElementById(\"Id\")"],   // PRETTTTTTIER!    CTRL + '
-    "getElementById(\"Id\")"
+    ['$("#Id")', 'getElement("Id")', 'gimmeEl("#Id")', 'getElementById("Id")'], // PRETTTTTTIER!
+    'getElementById("Id")'
+  ),
+  new QuizQuestion(
+    "Given two variables, which operator allows you to compare their values, as well as data type, to check if they are equal?",
+    ["==", "===", "=!=", "<=>"],
+    "==="
+  ),
+  new QuizQuestion(
+    "Given the following function definition, select the option that would allow you to invoke it. function halp(){console.log('Help me!')}",
+    ["run halp", "halp[]", "halp()", "eval halp"],
+    "halp()"
+  ),
+  new QuizQuestion(
+    "Given the function definition: function example(a, b){}, what are a and b called?",
+    ["Parameters", "Arguments"],
+    "Parameters"
+  ),
+  new QuizQuestion(
+    "Which of the following is NOT a data type in javascript?",
+    ["Boolean", "Decimal", "Object", "undefined"],
+    "Decimal"
+  ),
+  new QuizQuestion(
+    "Which value is falsy?",
+    ["1", "0.1", "undefined", "nah"],
+    "undefined"
+  ),
+  new QuizQuestion(
+    "Which character can break up a code line within a string?",
+    ["\\", "/", "|", "<!-- -->"],
+    "\\"
+  ),
+  new QuizQuestion(
+    "A variable declared without a value defined is assigned which value?",
+    [
+      "null",
+      "' ' (space character...but not this explanaition of the option)",
+      "undefined",
+      "NA",
+    ],
+    "undefined"
+  ),
+  new QuizQuestion(
+    "What characters identify an array in javascript?",
+    ["{}", "||", "[]", "<>"],
+    "[]"
+  ),
+  new QuizQuestion(
+    "As a best practice, where should global variables be declared?",
+    [
+      "Just before the first function declariation where its declared.",
+      "At the bottom of the .js file.",
+      "At the top of the .js file.",
+      "In a .js module, imported into any files where they are needed.",
+    ],
+    "At the top of the .js file."
+  ),
+  new QuizQuestion(
+    "What literal values can a boolean variable have?",
+    ["Stasis / Chaos", "Void / Creation", "False / True", "1 / 2"],
+    "False / True"
+  ),
+  new QuizQuestion(
+    "Given the array var alpha = ['a','b','c','d'], which option is a valid way to access element c?",
+    ["alpha[3]", "alpha[2]", "alpha.get(3)", "alpha.return(2)"],
+    "alpha[2]"
+  ),
+  new QuizQuestion(
+    "Using an explicit object definition, the object's properties are defined as _______ and _______.",
+    [
+      "name, value pairs",
+      "attribute, value pairs",
+      "var, thingy pairs",
+      "property var, value pairs",
+    ],
+    "name, value pairs"
+  ),
+  new QuizQuestion(
+    "Given an object's name, which character allows you to access its properties?",
+    [",", "-", ">", "."],
+    "."
+  ),
+  new QuizQuestion(
+    "What is the name of a block of code, defined globally, that will only run when called?",
+    ["method", "function", "doer", "exeggutor"],
+    "function"
+  ),
+  new QuizQuestion(
+    "Can a variable declared within a function be accessed outside of it?",
+    ["Yes", "No"],
+    "Yes"
+  ),
+  new QuizQuestion(
+    "Variables declared outside of a function are ________.",
+    ["universal", "titans", "global", "file-local"],
+    "global"
+  ),
+  new QuizQuestion(
+    "Which character is used for the modulus operator?",
+    ["/", "#", "%", "&"],
+    "%"
   ),
 ];
 const localStorageScoreKey = "jsQuizScores";
@@ -62,7 +162,8 @@ function handleQuizAnswer(event) {
 
 function handleSaveScore(event) {
   event.preventDefault();
-  var visitorScore = new VisitorScore($("initials").val().trim(), score);
+  console.log($("initials").val());
+  var visitorScore = new VisitorScore($("initials").val(), score);
   var currentScores = [];
   var localStorageScore = localStorage.getItem(localStorageScoreKey);
   if (localStorageScore) {
@@ -71,7 +172,8 @@ function handleSaveScore(event) {
   }
   currentScores.push(visitorScore);
   localStorage.setItem(localStorageScoreKey, JSON.stringify(currentScores));
-  changePageState(saveScoreEl, welcomeEl); //Redirect to high score page?
+  window.location.href = "./high-scores.html";
+  //changePageState(saveScoreEl, welcomeEl); ?Redirect to high score page works on G Pages?
 }
 
 function progressQuestions() {
